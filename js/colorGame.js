97113ad573d226aceb41f1105aab4870a9ed0155
elijahchen <code.elijahchen@gmail.com>
2017-02-25 13:43:57 -0500

    - Added function to randomly select a color from the temp array of colors to be the correct one

--- js/colorGame.js
+++ js/colorGame.js
@@ -4,15 +4,15 @@ var colors = [
 	"rgb(0, 255, 0)",
 	"rgb(0, 255, 255)",
 	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
+	"rgb(255, 0, 255)"
 ] //temp value, testing
 
-var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3]; //temp value, testing
-var colorDisplay = document.getElementById("colorDisplay");
-var messageDisplay = document.querySelector("#message");
+var squares = document.querySelectorAll(".square"); //Finds the class and stores it
+var pickColor = pickedColor(); //Function to determine correct color
+var colorDisplay = document.getElementById("colorDisplay"); //RGB header color
+var messageDisplay = document.querySelector("#message"); //Visual feedback: correct||wrong
 
-colorDisplay.textContent = pickedColor;
+colorDisplay.textContent = pickColor;
 
 for(var i = 0; i < squares.length; i++){
 	//Add initial colors to squares
@@ -23,11 +23,24 @@ for(var i = 0; i < squares.length; i++){
 		//Grab color of clicked squares
 		var clickedColor = this.style.background;
 		//Compare color to pickedColor
-		if(clickedColor === pickedColor){
-			alert("Correct");
+		if(clickedColor === pickColor){
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(color){
+	//Loop through all squares and change each color to match given color
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.background = color;
+	}
+}
+
+function pickedColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
